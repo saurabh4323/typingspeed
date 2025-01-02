b6c457ab43322c5e3984b866a22f749c74673d8c
Hrishita23 <hrishitasaha13@gmail.com>
2025-01-02 16:07:36 +0530

    gw

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,6 +1,18 @@
 import React from "react";
-import "./Login.css";
+import './Login.css'
 
 export default function Login() {
-  return <div></div>;
+  return <div className="container-login">
+    <div className="box-login"></div>
+    <div className="login">
+      <h1>Log In</h1>
+      <div className="form-login">
+        <p>Email</p>
+        <input type="text" placeholder="Email"></input>
+        <p>Password</p>
+        <input type="password" placeholder="Password"></input>
+      </div>
+      <button className="login-button">Log In</button>
+    </div>
+  </div>;
 }
